11104722a7173f71004f339c2853927535854d6b
mcube3567 <mcube3567@gmail.com>
2020-01-15 10:38:20 +0100

    backend

--- learningredux.js
+++ learningredux.js
@@ -304,3 +304,27 @@ HANDLING EVENTS WITH REACT JS
 
  
 */
+//conditional rendering
+/*
+in react  can create distinct components tat escapsulate behavior needed then render only some of them depending on hte state of the application
+consider these two components
+*/
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+//create a greeting component that displays either of te above component depending on whether a user is logged in
+function Greeting(props){
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn){
+        return <UserGreeting/>
+    }
+    return <GuestGreeting/>
+}
+ReactDOM.render(
+    <Greeting isLoggedIn={true}/>,
+    document.getElementById('root')
+);
