7237f6605c7aa7b9a2dcab2bfb6316f3bde4a104
mcube3567 <mcube3567@gmail.com>
2020-01-28 12:04:11 +0100

    database creation

---
+++ data.js
@@ -0,0 +1,19 @@
+/*
+database prevents data from being reset wen the ap resets
+it is a collection of data organised in a useful way
+relational databases e.g
+mysql, postgresql
+all relational databases use sql
+wit sql u can communicate wit the database
+//non relational databse(no sql) e.g
+mongoDB
+lets u buildd an app without aving to define a schemer
+e.g  pre built (document related)
+te language that connects mongoDB to te app is MongoDB query.
+
+
+//postgres installation and use
+open cmd
+psql enter not recognised
+
+*/
