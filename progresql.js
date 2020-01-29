af2637511e470ad402692095341c1bb0e7a55662
mcube3567 <mcube3567@gmail.com>
2020-01-29 13:01:53 +0100

    database 2

---
+++ progresql.js
@@ -0,0 +1,17 @@
+/*
+cmd for psql commands
+psql -U postgres
+insert password
+CREATE DATABASE {Name e.g test}
+test~# CREATE TABLE users(name text, age smallint, birthday date);
+INSERT INTO users (name, age, birthday) VALUES ('mo',1, 1900-01-22);
+
+\c test
+SELECT * FROM users (to grab te data)
+
+to add anoter column to a pre existing table
+u can use te ALTER format
+e.g ALTER TABLE users 
+
+*/
+//connecting the frontend and backend smart brain project
